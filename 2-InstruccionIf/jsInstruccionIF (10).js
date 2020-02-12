6689b1e283960b612e6de34267653337d3c95a6e
gustavo <gustavo@example.com>
2020-02-11 21:10:51 -0300

    ramdom

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,26 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var random
+
+	random=Math.floor((Math.random() * 10) + 1);
+
+	console.log(random)
+
+	if(random>8)
+	{
+		alert("Excelente")
+	}
+	else
+		if(random<5)
+		{
+			alert("Vamos, la proxima se puede")
+		}
+		else 
+		{
+			alert("APROBO")
+		}
+			
+
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -2,7 +2,18 @@ function mostrar()
 {
 //tomo la edad  
 
-	
+	var edad
+
+	if(edad<18 && estadoCivil!="Soltero")
+	{
+
+	}
+	else
+	{
+		if (estadoCivil=="Soltero" && edad>17)
+
+			alert("Es soltero y no es menor");
+	}
 
 
 }//FIN DE LA FUNCIÓN
