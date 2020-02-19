3df61561cba44e6bac87076b88e9194cf04f1838
bijou84 <60628772+bijou84@users.noreply.github.com>
2020-02-18 22:02:07 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -2,8 +2,11 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
+switch(mesDelaño)
+{
+	case "Enero":
+		console.log("que comiences bien el año");
 
 
-
-
+}
 }//FIN DE LA FUNCIÓN
