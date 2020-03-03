961c6fbc9cf51f5c85c2b06be2b3cc21c77f478e
bijou84 <60628772+bijou84@users.noreply.github.com>
2020-03-02 22:22:39 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -2,11 +2,21 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
+switch(mesDelAño)
+{
+	case "Febrero":
+		console.log("Tiene 28 días")
+		break;
 
-//alert (mesDelAño);
-	
-	
+	case "Abril":
+	case "Junio":
+	case "Septiembre":
+	case "Noviembre":
+		console.log("Tiene 30 días")
+		break;
 
-
-
+	default:
+		console.log("Tiene 31 días")
+		break;
+}
 }//FIN DE LA FUNCIÓN
